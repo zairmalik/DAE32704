00e933193551635641468273482cf543ed44bba6
Uzair <31562068+zairmalik@users.noreply.github.com>
2018-09-20 19:23:42 +0800

    About activity

---
+++ Android/about_app.kt
@@ -0,0 +1,12 @@
+package com.learn.uzair.videosurveillanceapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class about_app : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_app)
+    }
+}
