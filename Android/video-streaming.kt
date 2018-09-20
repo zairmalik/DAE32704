59298bbacf269c9903c818758685f17d6a0cdb17
Uzair <31562068+zairmalik@users.noreply.github.com>
2018-09-20 19:21:19 +0800

    Main activity

---
+++ Android/video-streaming.kt
@@ -0,0 +1,12 @@
+package com.learn.uzair.videosurveillanceapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class video_streaming : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_streaming)
+    }
+}
